30285f79f7b2b6ba0b97a2fe64da212aea7c4e6f
Denis <Savchenkodkz@gmail.com>
2022-11-26 03:31:04 +0600

    Задача 15

--- Program.cs
+++ Program.cs
@@ -1,19 +1,12 @@
-﻿Console.WriteLine("Введите трехзначное число");
+﻿Console.WriteLine("Введите цифрой день недели");
 int a = Convert.ToInt32(Console.ReadLine());
-int a1 = 0;
-int a2 = 0;
-    if (a < 100 ){
-        Console.WriteLine("Вы ввели некорректное число");
+
+    if (a == 6){
+        Console.WriteLine("Этот день является выходным");
     }
-    else if (a > 999){
-        Console.WriteLine("Вы ввели некоректное число");
+    else if (a == 7){
+        Console.WriteLine("Этот день является выходным");
     }
     else {
-         a1 = a / 100;
-         a2 = a % 100;
-         a2 = a2 / 10;
+        Console.WriteLine("Этот день не является выходным");
     }
-
-        Console.Write($"Вторая цифра числа {a}: {a2}");
-    
-
