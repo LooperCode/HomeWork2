bf981bb97693c0c4029bc23c329d1a44affc61b5
Denis <Savchenkodkz@gmail.com>
2022-11-26 07:00:44 +0600

    увеличение интервала

--- Program.cs
+++ Program.cs
@@ -1,11 +1,12 @@
-﻿Console.WriteLine("Введите минимум трехзначное число");
+﻿Console.WriteLine("Введите минимум трехзначное число до ''999999''");
 int a = Convert.ToInt32(Console.ReadLine());
 int a1 = 0;
-int a2 = 0;
+
     if (a < 100 ){
         Console.WriteLine("Третьей цифры нет");
     
     }
+    
     else if (a <= 999){
         
         a1 = a % 10;
@@ -17,12 +18,17 @@ int a2 = 0;
         Console.Write($"Третья цифра числа {a}: {a1}");
     }    
         
-    else {
-         a1 = a % 1000;
-         a1 = a1 / 100;
+    else if (a <= 99999) {
+         
+         a1 = a / 100;
+         a1 = a1 % 10;
         Console.Write($"Третья цифра числа {a}: {a1}");
     }
 
-        
+     else {
+        a1 = a / 1000;
+        a1 = a1 % 10;
+        Console.Write($"Третья цифра числа {a}: {a1}");
+     }   
     
 
