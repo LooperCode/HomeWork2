737e7514604c7ecb275f37ad7c2427c9412389ae
Denis <Savchenkodkz@gmail.com>
2022-11-29 23:05:26 +0600

    Упрощение алгоритма с помощтю цикла

--- Program.cs
+++ Program.cs
@@ -1,34 +1,20 @@
-﻿Console.WriteLine("Введите минимум трехзначное число до ''999999''");
+﻿Console.WriteLine("Введите минимум трехзначное число");
 int a = Convert.ToInt32(Console.ReadLine());
 int a1 = 0;
-
-    if (a < 100 ){
+int temp = a;
+    if (a <= 99 ){
         Console.WriteLine("Третьей цифры нет");
     
     }
-    
-    else if (a <= 999){
-        
+    else if (a < 999){
         a1 = a % 10;
-        Console.Write($"Третья цифра числа {a}: {a1}");
     }
-    else if (a <= 9999) {
-        a1 = a % 100;
-        a1 = a1 / 10;
-        Console.Write($"Третья цифра числа {a}: {a1}");
-    }    
-        
-    else if (a <= 99999) {
-         
-         a1 = a / 100;
-         a1 = a1 % 10;
-        Console.Write($"Третья цифра числа {a}: {a1}");
-    }
-
-     else {
-        a1 = a / 1000;
-        a1 = a1 % 10;
-        Console.Write($"Третья цифра числа {a}: {a1}");
-     }   
     
-
+    else {
+        while (a > 999) {
+            a = a / 10;
+            a1 = a%10;
+        }
+    }
+Console.WriteLine($"Третья цифра числа {temp}: {a1}");
+    
